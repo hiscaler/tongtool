3024639130596ee58724adf5d04d6f8c187be0c0
hiscaler <hiscaler@gmail.com>
2021-12-28 13:42:58 +0800

    ...

--- erp2/tracking.number.go
+++ erp2/tracking.number.go
@@ -27,6 +27,7 @@ type TrackingNumberQueryParams struct {
 
 // TrackingNumbers 订单物流单号列表
 // 需要注意的是该封装总是返回包含所有查询订单集合的数据，无论是否有物流数据
+// https://open.tongtool.com/apiDoc.html#/?docId=3b3cceec8fe04e6db44da17ec4b38f08
 func (s service) TrackingNumbers(params TrackingNumberQueryParams) (items []TrackingNumber, isLastPage bool, err error) {
 	params.MerchantId = s.tongTool.MerchantId
 	items = make([]TrackingNumber, 0)
--- erp2/tracking.number_test.go
+++ erp2/tracking.number_test.go
@@ -1,13 +1,15 @@
 package erp2
 
 import (
+	"github.com/hiscaler/tongtool/pkg/cast"
 	"testing"
 )
 
 func TestService_TrackingNumbers(t *testing.T) {
 	_, ttService := newTestTongTool()
+	// 验证返回结果数量
 	params := TrackingNumberQueryParams{}
-	params.OrderIds = []string{"bad.order.id"}
+	params.OrderIds = []string{"bad.order.id1", "bad.order.id2", "L-M20211221152430918"}
 	trackingNumbers := make([]TrackingNumber, 0)
 	for {
 		pageTrackingNumbers, isLastPage, err := ttService.TrackingNumbers(params)
@@ -22,10 +24,12 @@ func TestService_TrackingNumbers(t *testing.T) {
 		params.PageNo++
 	}
 	if len(trackingNumbers) != len(params.OrderIds) {
-		t.Errorf("order")
+		t.Errorf("except return %d records, actual return %d records", len(params.OrderIds), len(trackingNumbers))
+	} else {
+		t.Log(cast.ToJson(trackingNumbers))
 	}
 
-	// 未提供订单集合参数
+	// 验证未提供订单集合参数
 	_, _, err := ttService.TrackingNumbers(TrackingNumberQueryParams{})
 	if err != nil {
 		t.Errorf("ttService.TrackingNumbers error: %s", err.Error())
