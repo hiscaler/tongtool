4c6a65daebc8a7b8b9f6644dd293b02a4a635049
hiscaler <hiscaler@gmail.com>
2021-12-30 00:14:45 +0800

    ...

--- erp2/purchase.order.go
+++ erp2/purchase.order.go
@@ -262,3 +262,54 @@ func (s service) PurchaseOrderStockInLogs(params PurchaseOrderLogQueryParams) (i
 	}
 	return
 }
+
+// 采购单到货处理
+// https://open.tongtool.com/apiDoc.html#/?docId=ee942453af114a7686d0c8d5187988f2
+
+// PurchaseOrderArrivalItem 采购到货项
+type PurchaseOrderArrivalItem struct {
+	ArrivalGoodsList  []PurchaseOrderArrivalGoodsItem `json:"arrivalGoodsList"`  // 采购到货明细
+	Freight           float64                         `json:"freight"`           // 运费
+	PurchaseOrderCode string                          `json:"purchaseOrderCode"` // 采购单号
+	Remark            string                          `json:"remark"`            // 到货备注
+}
+
+// PurchaseOrderArrivalGoodsItem 采购到货项
+type PurchaseOrderArrivalGoodsItem struct {
+	GoodsDetailId        string `json:"goodsDetailId"`        // 通途货品ID
+	InQuantity           int    `json:"inQuantity"`           // 到货数量
+	IsReplace            string `json:"isReplace"`            // 是否是变参替换到货：[Y：是]
+	ReplaceGoodsDetailId string `json:"replaceGoodsDetailId"` // 变参替换的通途货品ID
+	ReplaceQuantity      int    `json:"replaceQuantity"`      // 变参替换的到货数量
+}
+
+type PurchaseOrderArrivalRequest struct {
+	MerchantId          string                     `json:"merchantId"`          // 商户ID
+	PurchaseArrivalList []PurchaseOrderArrivalItem `json:"purchaseArrivalList"` // 采购到货列表
+}
+
+// PurchaseOrderArrival 采购单到货
+func (s service) PurchaseOrderArrival(req PurchaseOrderArrivalRequest) error {
+	req.MerchantId = s.tongTool.MerchantId
+	res := struct {
+		result
+		Datas interface{} `json:"datas"`
+	}{}
+	resp, err := s.tongTool.Client.R().
+		SetBody(req).
+		SetResult(&res).
+		Post("/openapi/tongtool/purchaseArrival")
+	if err == nil {
+		if resp.IsSuccess() {
+			err = tongtool.ErrorWrap(res.Code, res.Message)
+		} else {
+			if e := json.Unmarshal(resp.Body(), &res); e == nil {
+				err = tongtool.ErrorWrap(res.Code, res.Message)
+			} else {
+				err = errors.New(resp.Status())
+			}
+		}
+	}
+
+	return err
+}
--- erp2/purchase.order_test.go
+++ erp2/purchase.order_test.go
@@ -95,3 +95,29 @@ func TestService_PurchaseOrderStockInLogs(t *testing.T) {
 		params.PageNo++
 	}
 }
+
+func TestService_PurchaseOrderArrival(t *testing.T) {
+	_, ttService := newTestTongTool()
+	req := PurchaseOrderArrivalRequest{
+		PurchaseArrivalList: []PurchaseOrderArrivalItem{
+			{
+				ArrivalGoodsList: []PurchaseOrderArrivalGoodsItem{
+					{
+						GoodsDetailId:        "123",
+						InQuantity:           1,
+						IsReplace:            "N",
+						ReplaceGoodsDetailId: "",
+						ReplaceQuantity:      0,
+					},
+				},
+				Freight:           1,
+				PurchaseOrderCode: "PO123",
+				Remark:            "备注",
+			},
+		},
+	}
+	err := ttService.PurchaseOrderArrival(req)
+	if err != nil {
+		t.Errorf("ttService.PurchaseOrderArrival error: %s", err.Error())
+	}
+}
