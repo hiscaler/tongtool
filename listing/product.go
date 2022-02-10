2cace24493797633398e69124e64b37831c7d018
hiscaler <hiscaler@gmail.com>
2022-02-10 16:39:00 +0800

    ...

--- listing/product.go
+++ listing/product.go
@@ -131,15 +131,136 @@ type ProductVideoGallery struct {
 	VideoList           []ProductVideo `json:"videoList"`           // 视频列表
 }
 
+// ProductGoodsVariation 售卖商品多属性信息
+type ProductGoodsVariation struct {
+	GoodsDetailId     string `json:"goodsDetailId"`     // 货品顺序号
+	GoodsVariationId  string `json:"goodsVariationId"`  // 商品属性顺序号
+	MerchantId        string `json:"merchantId"`        // 商户编号
+	SortNo            int    `json:"sortNo"`            // 排序号
+	VariationName     string `json:"variationName"`     // 属性名称英文US
+	VariationNameAU   string `json:"variationNameAU"`   // 属性名英语AU
+	VariationNameCHT  string `json:"variationNameCHT"`  // 属性名中文
+	VariationNameFra  string `json:"variationNameFra"`  // 属性名称法语
+	VariationNameGer  string `json:"variationNameGer"`  // 属性名称德语
+	VariationNameIt   string `json:"variationNameIt"`   // 属性名称意大利语
+	VariationNamePol  string `json:"variationNamePol"`  // 属性名称波兰语
+	VariationNamePor  string `json:"variationNamePor"`  // 属性名称葡萄牙语
+	VariationNameSpn  string `json:"variationNameSpn"`  // 属性名称西班牙语
+	VariationNameUK   string `json:"variationNameUK"`   // 属性名英语uk
+	VariationValue    string `json:"variationValue"`    // 属性值英文US
+	VariationValueAU  string `json:"variationValueAU"`  // 属性值英语AU
+	VariationValueCHT string `json:"variationValueCHT"` // 属性值中文
+	VariationValueFra string `json:"variationValueFra"` // 属性值法语
+	VariationValueGer string `json:"variationValueGer"` // 属性值德语
+	VariationValueIt  string `json:"variationValueIt"`  // 属性值意大利语
+	VariationValuePol string `json:"variationValuePol"` // 属性值波兰语
+	VariationValuePor string `json:"variationValuePor"` // 属性值葡萄牙语
+	VariationValueSpn string `json:"variationValueSpn"` // 属性值西班牙语
+	VariationValueUK  string `json:"variationValueUK"`  // 属性值英语uk
+}
+
+// ProductStockGoodsInfo 关联库存
+type ProductStockGoodsInfo struct {
+	GoodsDetailId      string `json:"goodsDetailId"`      // 货品顺序号
+	MappingId          string `json:"mappingId"`          // 关联序列号
+	MerchantId         string `json:"merchantId"`         // 商户编号
+	Quantity           int    `json:"quantity"`           // 数量
+	SortNo             int    `json:"sortNo"`             // 排序号
+	StockGoodsDetailId string `json:"stockGoodsDetailId"` // 库存货品顺序号
+}
+
+// ProductGoodsInfo 售卖商品多属性信息
+type ProductGoodsInfo struct {
+	DistributeGoodsDetailId string                  `json:"distributeGoodsDetailId"` // 分销货品顺序号
+	GoodHeight              float64                 `json:"goodHeight"`              // 货品高度
+	GoodLength              float64                 `json:"goodLength"`              // 货品长度
+	GoodPurchaseCost        string                  `json:"goodPurchaseCost"`        // 货品采购成本
+	GoodWeight              int                     `json:"goodWeight"`              // 货品重量 (克)
+	GoodWidth               float64                 `json:"goodWidth"`               // 货品宽度
+	GoodsDetailId           string                  `json:"goodsDetailId"`           // 货品顺序号
+	GoodsVariationList      []ProductGoodsVariation `json:"goodsVariationList"`      // 售卖商品多属性信息
+	MerchantId              string                  `json:"merchantId"`              // 商户编号
+	ProductId               string                  `json:"productId"`               // 图片组顺序号
+	SKU                     string                  `json:"sku"`                     // 属性SKU
+	SortNo                  int                     `json:"sortNo"`                  // 排序号
+	StockGoodsDetailId      string                  `json:"stockGoodsDetailId"`      // 售卖货品顺序号
+	StockGoodsInfos         []ProductStockGoodsInfo `json:"stockGoodsInfos"`         // 关联库存
+	SupplierSKU             string                  `json:"supplierSku"`             // supplierSku
+}
+
+// ProductImage 商品图片
+type ProductImage struct {
+	ImageAddress         string `json:"imageAddress"`         // 图片地址
+	ImageType            string `json:"imageType"`            // 图片类型(W-橱窗图,D-描述图)
+	IsUploadedAbroad     string `json:"isUploadedAbroad"`     // 是否已上传国外服务器
+	MerchantId           string `json:"merchantId"`           // 商户编号
+	OriginalImageAddress string `json:"originalImageAddress"` // 原始图片地址
+	ProductImageGroupId  string `json:"productImageGroupId"`  // 图片组顺序号
+	ProductImageId       string `json:"productImageId"`       // 图片顺序号
+	SortNo               int    `json:"sortNo"`               // 图片顺序
+}
+
+// ProductImageGroup 图片分组
+type ProductImageGroup struct {
+	ImageGroupType      string         `json:"imageGroupType"`      // 图片组类型,A-橱窗图,D-描述图(详情图)，V-多属性图,L-图片组列表,
+	MerchantId          string         `json:"merchantId"`          // 商户编号
+	ProductId           string         `json:"productId"`           // 图片组顺序号
+	ProductImageGroupId string         `json:"productImageGroupId"` // 图片组顺序号
+	ProductImageList    []ProductImage `json:"productImageList"`    // 图片列表
+	SaleAccountIds      string         `json:"saleAccountIds"`      // 适用平台与账号
+	SortNo              int            `json:"sortNo"`              // 图片组顺序
+}
+
+// ProductLabelName 标签
+type ProductLabelName struct {
+	LabelName      string `json:"labelName"`      // 规格名称
+	MerchantId     string `json:"merchantId"`     // 商户编号
+	ProductId      string `json:"productId"`      // 产品顺序号
+	ProductLabelId string `json:"productLabelId"` // 商品标签顺序号
+}
+
+// ProductMonitor 来源
+type ProductMonitor struct {
+	MerchantId       string `json:"merchantId"`       // 商户编号
+	MonitorLink      string `json:"monitorLink"`      // 来源URL
+	ProductId        string `json:"productId"`        // 库存产品编号
+	ProductMonitorId string `json:"productMonitorId"` // 库存产品来源URL编号
+}
+
+// ProductNote 备注
+type ProductNote struct {
+	Content       string `json:"content"`       // 备注内容
+	MerchantId    string `json:"merchantId"`    // 商户编号
+	ProductId     string `json:"productId"`     // 产品顺序号
+	ProductNoteId string `json:"productNoteId"` // 产品备注顺序号
+}
+
+// ProductVariationImage 主属性图片列表
+type ProductVariationImage struct {
+	ImageAddress   string `json:"imageAddress"`   // 图片地址
+	ImageId        string `json:"imageId"`        // 顺序号
+	MerchantId     string `json:"merchantId"`     // 商户编号
+	ProductId      string `json:"productId"`      // 产品顺序号
+	SortNo         int    `json:"sortNo"`         // 图片顺序
+	VariationValue string `json:"variationValue"` // 主属性值
+}
+
 // Product 售卖资料
 type Product struct {
 	BaseInfo            ProductBaseInfo          `json:"baseInfo"`            // BaseInfo
 	CustomAttributeList []ProductCustomAttribute `json:"customAttributeList"` // 自定义属性
 	DescribeList        []ProductDescription     `json:"describeList"`        // 描述和标题
 	GalleryVideoList    []ProductVideoGallery    `json:"galleryVideoList"`    // 视频组
+	GoodsInfoList       []ProductGoodsInfo       `json:"goodsInfoList"`       // 售卖商品多属性信息
+	ImageGroupList      []ProductImageGroup      `json:"imageGroupList"`      // 图片分组列表
+	ImageList           []ProductImageGroup      `json:"imageList"`           // 图片列表
+	LabelNames          []ProductLabelName       `json:"labelNames"`          // 标签
+	MonitorList         []ProductMonitor         `json:"monitorList"`         // 来源
+	NoteList            []ProductNote            `json:"noteList"`            // 备注
+	VariationImagesList []ProductVariationImage  `json:"variationImagesList"` // 主属性图片列表
 }
 
-// UpdateProductRequest Todo 结构不完整，需要完善
+// UpdateProductRequest 商品更新请求
 type UpdateProductRequest struct {
 	MerchantId           string    `json:"merchantId"`           // 商户号
 	ProductInfoParamList []Product `json:"productInfoParamList"` // 售卖资料
