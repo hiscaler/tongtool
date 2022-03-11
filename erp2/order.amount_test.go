688565c76ade6b63d116ba19051f11df7916166f
hiscaler <hiscaler@gmail.com>
2022-03-11 14:49:56 +0800

    ...

--- erp2/order.amount_test.go
+++ erp2/order.amount_test.go
@@ -1,27 +1,37 @@
 package erp2
 
 import (
+	"errors"
 	"fmt"
 	"github.com/hiscaler/gox/jsonx"
+	"github.com/hiscaler/tongtool"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
 func TestOrderAmount(t *testing.T) {
-	order := Order{
-		OrderIdCode:               "O123",
-		ShippingFee:               10,
-		ShippingFeeIncome:         5,
-		ShippingFeeIncomeCurrency: USD,
+	orderNumber := "O1234567"
+	order, _, err := ttService.Order(orderNumber)
+	if err != nil {
+		if errors.Is(err, tongtool.ErrNotFound) {
+			t.Errorf("%s not exists in tongtool", orderNumber)
+		} else {
+			t.Errorf("ttService.Order error: %s", err.Error())
+		}
+	} else if !strings.EqualFold(order.OrderIdCode, orderNumber) {
+		t.Errorf("order.OrderIdKey %s not match %s", order.OrderIdCode, orderNumber)
+	} else {
+		orderAmount := NewOrderAmount(order, map[string]float64{
+			USD: 6.3927,
+			CNY: 1,
+		}, 2, 22.45)
+		assert.Equal(t, 8.35, orderAmount.Summary.Expenditure, "order 1")
+		assert.Equal(t, 98.72, orderAmount.Summary.Income, "order 2")
+		newOrder, err := orderAmount.ExchangeTo(USD)
+		assert.Equal(t, nil, err, "newOrder 1")
+		fmt.Println(jsonx.ToPrettyJson(orderAmount))
+		fmt.Println(jsonx.ToPrettyJson(newOrder))
 	}
-	orderAmount := NewOrderAmount(order, map[string]float64{
-		USD: 0.1587,
-		CNY: 1,
-	}, 2)
-	assert.Equal(t, 14.73, orderAmount.TotalExpenditureAmount, "order 1")
-	assert.Equal(t, 31.51, orderAmount.IncomeAmount.Shipping, "order 2")
-	newOrder, err := orderAmount.ExchangeTo(USD)
-	assert.Equal(t, nil, err, "newOrder 1")
-	fmt.Println(jsonx.ToPrettyJson(orderAmount))
-	fmt.Println(jsonx.ToPrettyJson(newOrder))
+
 }
