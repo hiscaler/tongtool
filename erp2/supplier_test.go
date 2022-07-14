b7b5271e9c19c7567f4dc90f9107c78828841015
hiscaler <hiscaler@gmail.com>
2022-07-14 09:48:29 +0800

    ...

--- erp2/supplier_test.go
+++ erp2/supplier_test.go
@@ -1,6 +1,7 @@
 package erp2
 
 import (
+	"github.com/hiscaler/gox/jsonx"
 	"strings"
 	"testing"
 )
@@ -11,19 +12,21 @@ func TestService_Suppliers(t *testing.T) {
 	params.PageNo = 1
 	name := "栀子花开女装店"
 	found := false
+	var supplier Supplier
 	for {
 		suppliers, isLastPage, err := ttService.Suppliers(params)
-		if err == nil {
-			for _, supplier := range suppliers {
-				if strings.EqualFold(name, supplier.CorporationFullName) {
-					found = true
-					break
-				}
-			}
-		} else {
-			t.Error(err)
-			break
+		if err != nil {
+			t.Errorf("suppliers error: %s", err.Error())
 		}
+
+		for _, v := range suppliers {
+			if strings.EqualFold(name, v.CorporationFullName) {
+				found = true
+				supplier = v
+				break
+			}
+		}
+
 		if isLastPage || found {
 			break
 		}
@@ -32,4 +35,5 @@ func TestService_Suppliers(t *testing.T) {
 	if !found {
 		t.Errorf("%s not found", name)
 	}
+	t.Log(jsonx.ToJson(supplier, "{}"))
 }
